b863e6ffd19c9148a1d3cc64a5bed02b3ddeee31
Rudolf Jan <rudolfjan@klantprocessenboek.nl>
2020-04-29 10:44:14 +0200

    Fixed syntax error in Updata ServiceInstance in database

--- TimeTableTool.Desktop/Views/LocationView.xaml.cs
+++ TimeTableTool.Desktop/Views/LocationView.xaml.cs
@@ -12,15 +12,27 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TimetableTool.Desktop.Views
-  {
-  /// <summary>
-  /// Interaction logic for LocationView.xaml
-  /// </summary>
-  public partial class LocationView : UserControl
-    {
-    public LocationView()
-      {
-      InitializeComponent();
-      }
-    }
-  }
+	{
+	/// <summary>
+	/// Interaction logic for LocationView.xaml
+	/// </summary>
+	public partial class LocationView : UserControl
+		{
+		private int rowCount;
+		public LocationView()
+			{
+			InitializeComponent();
+			}
+
+		//Scroll to last row if a row is added
+		private void OnLoadingRow(object sender, DataGridRowEventArgs e)
+			{
+			if (LocationList.Items.Count > rowCount)
+				{
+				var lastRow = LocationList.Items[^1];
+				LocationList.ScrollIntoView(lastRow);
+				rowCount = LocationList.Items.Count;
+				}
+			}
+		}
+	}
